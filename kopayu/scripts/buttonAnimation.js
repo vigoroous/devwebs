6ba845bc7d6e4d02c7469d118e00b9b6a6a562d7
vigoroous <vigoroous@gmail.com>
2016-05-22 16:17:56 +0300

    Add files via upload

---
+++ kopayu/scripts/buttonAnimation.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+    $(".button").mouseenter(function () {
+        $(".line1").animate({ width: "10%" }, 1)
+        $(".line2").animate({ width: "10%" }, 1)
+        $(".line3").animate({ width: "10%" }, 100)
+        $(".line4").animate({ width: "10%" }, 100)
+        $(".button p.visible").animate({ "letter-spacing": "+=0.03em" }, 400)
+    });
+    $(".button").mouseleave(function () {
+        if ($(".button p.visible").is(':animated')) {
+            $(".button").find("p.visible").stop(true, true)
+            $(".button p.visible").animate({ "letter-spacing": "-0.53px" }, 400)
+        }
+        else {
+            $(".button p.visible").animate({ "letter-spacing": "-=0.03em" }, 400)
+        }        
+        $(".line1").animate({ width: "51%" }, 1)
+        $(".line2").animate({ width: "51%" }, 1)
+        $(".line3").animate({ width: "51%" }, 100)
+        $(".line4").animate({ width: "51%" }, 100)
+    });
+    return false;
+});
