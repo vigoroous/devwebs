37bb957165ec81865cb35ff75f3be1e3d5ea8386
vigoroous <vigoroous@gmail.com>
2016-05-22 16:21:47 +0300

    Add files via upload

---
+++ inground/cartwidget/rotation.js
@@ -0,0 +1,32 @@
+﻿$.fn.animateRotate = function (angle, duration, easing, complete) {
+    return this.each(function () {
+        var $elem = $(this);
+
+        $({ deg: 0 }).animate({ deg: angle }, {
+            duration: 800,
+            easing: easing,
+            step: function (now) {
+                $elem.css({
+                    transform: 'rotate(' + now + 'deg)'
+                });
+            },
+            complete: complete || $.noop
+        });
+    });
+};
+$.fn.animateRotate2 = function (angle, duration, easing, complete) {
+    return this.each(function () {
+        var $elem = $(this);
+
+        $({ deg: 180 }).animate({ deg: angle }, {
+            duration: 800,
+            easing: easing,
+            step: function (now) {
+                $elem.css({
+                    transform: 'rotate(' + now + 'deg)'
+                });
+            },
+            complete: complete || $.noop
+        });
+    });
+};
